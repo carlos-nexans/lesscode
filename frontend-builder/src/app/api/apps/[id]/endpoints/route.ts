f4c2232d1c2d4cbb2dd497892a103a3424481f18
Carlos David Gonzalez Nexans <cdgn.machines@gmail.com>
2024-06-30 12:46:55 -0300

    fix: add executor api

--- frontend-builder/src/app/api/apps/[id]/endpoints/route.ts
+++ frontend-builder/src/app/api/apps/[id]/endpoints/route.ts
@@ -1,7 +1,5 @@
-import {addEndpoint, addWorkflow, editEndpoint} from "@/repository/apps";
 import {Workflow} from "executable-workflows";
-import {storeWorkflowDefinition} from "@/repository/workflows";
-
+import {addEndpoint} from "@/repository/apps";
 export const POST = async function (req, { params }) {
     const id = params.id;
     const data = await req.json()
